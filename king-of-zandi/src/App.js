9e4e4f6374fdd42cd89581825c06dd262344813d
Dawon00 <dawon000203@gmail.com>
2023-12-31 01:41:54 +0900

    Add MemberDetail page and update App.js

--- king-of-zandi/src/App.js
+++ king-of-zandi/src/App.js
@@ -1,19 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-
+import MemberDetail from "./pages/MemberDetail";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-      </Routes>
-    </div>
-
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/member/:memberId" element={<MemberDetail />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- king-of-zandi/tailwind.config.js
+++ king-of-zandi/tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-
-    extend: {
-      screens: {
-        sm: '430px', // 핸드폰 뷰
-        md: '768px',
-      },
-      colors: {
-        pointColor: "#60D68A",
-        bgColor: "#242625",
-        dummyColor: "#999999"
-      },
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                sm: "430px", // 핸드폰 뷰
+                md: "768px",
+            },
+            colors: {
+                pointColor: "#60D68A",
+                bgColor: "#242625",
+                divideColor: "#737373",
+                dummyColor: "#999999",
+            },
+        },
     },
-  },
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
 };
