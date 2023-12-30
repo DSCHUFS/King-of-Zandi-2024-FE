d0b0ae0bc9d118165ab11aa038454567b5fc33da
juhui88 <sand8594@naver.com>
2023-12-30 16:01:11 +0900

    [FE-02]feat:TabBar.js

--- king-of-zandi/src/pages/Home.js
+++ king-of-zandi/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import TabBar from '../components/TabBar';
 
 const Home = () => {
     return (
         <div>
+            <TabBar />
         </div>
     );
 };
