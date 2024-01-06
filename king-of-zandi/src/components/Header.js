526ec47bb476464cf4017bb3acc5cb18f7013841
juhui88 <sand8594@naver.com>
2024-01-06 12:41:43 +0900

    [FE-07] fix:Header

--- king-of-zandi/src/components/Header.js
+++ king-of-zandi/src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+    const location = useLocation()
+    console.log(location)
     return (
         <div className='bg-bgColor flex justify-between items-center px-4 md:px-20 py-5 sm:py-3'>
             <div className='space-x-2'>
                 <Link to="/" className='text-xl sm:text-[26px] font-bold'>잔디왕 김훕스</Link>
                 <span className='hidden sm:inline-block text-base text-dummyColor'>Powered By GDSC HUFS</span>
             </div>
-            <div>
-                <img className='cursor-pointer w-10 h-10 sm:w-auto sm:h-auto' src='imgs/table.png' />
-            </div>
+            {location.pathname == "/" ?
+                <div className=' w-7'>
+                    <img className='cursor-pointer w-5 h-5 object-cover sm:w-auto sm:h-auto' src='/imgs/table.png' />
+                </div> : null}
+
         </div>
     );
 };
