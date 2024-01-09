1f7686cfee72585b4a164f790b98b236f87addb7
s00ngle <dydtns9815@naver.com>
2024-01-09 16:40:11 +0900

    [FE-16] fix ProfileListItem size

--- king-of-zandi/src/components/ProfileListBox.js
+++ king-of-zandi/src/components/ProfileListBox.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { ReactComponent as Profile } from "../assets/image/profile.svg";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const ProfileListBox = ({ name, last28, githubUsername }) => {
     return (
-        <Link to={`/member/${githubUsername}`} className="flex justify-between items-center m-3 p-2 md:p-2 bg-bgColor h-20 md:h-16 rounded-lg">
+        <Link
+            to={`/member/${githubUsername}`}
+            className="flex justify-between items-center m-3 p-2 md:p-2 bg-bgColor h-20 md:h-16 rounded-lg"
+        >
             <div className="flex items-center">
-                <img src={`https://github.com/${githubUsername}.png`} className="w-8 mr-3 h-8 md:w-8 md:h-8 text-nowrap rounded-full" />
+                <img
+                    src={`https://github.com/${githubUsername}.png`}
+                    className="w-8 mr-3 h-8 md:w-8 md:h-8 text-nowrap rounded-full"
+                />
                 <div className="font-extrabold text-base text-nowrap mr-10">
                     {name}
                 </div>
@@ -16,7 +22,9 @@ const ProfileListBox = ({ name, last28, githubUsername }) => {
                     {last28.map((i, index) => (
                         <div
                             key={index}
-                            className={`w-5 h-5 ${i === 0 ? "bg-white" : "bg-pointColor"} rounded-sm m-[2px]`}
+                            className={`w-5 h-5 ${
+                                i === 0 ? "bg-white" : "bg-pointColor"
+                            } rounded-sm m-[2px]`}
                         ></div>
                     ))}
                 </div>
