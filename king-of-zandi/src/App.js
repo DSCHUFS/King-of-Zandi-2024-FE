ab1e4b06fedb6ed7b76f3bfab621100ced7d4008
Dawon00 <dawon000203@gmail.com>
2024-01-09 15:06:38 +0900

    Add 404 page redirect

--- king-of-zandi/src/App.js
+++ king-of-zandi/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import MemberDetail from "./pages/MemberDetail";
@@ -13,6 +13,7 @@ function App() {
                     path="/member/:githubProfileID"
                     element={<MemberDetail />}
                 />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </div>
     );
